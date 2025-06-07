de585dcc8e53d671c8ee4d260055a8dcd2f907e9
parth-vaghela005 <vaghelaparth300@gmail.com>
2025-06-07 21:07:59 +0530

    feat: enhance Auth and Posts routes with user profile and suggestion functionalities

--- routes/Auth-routes.js
+++ routes/Auth-routes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import {CreateUser, LoginUser  , Logout} from '../controllers/User.Controller.js'
+import {CreateUser, LoginUser  , Logout ,GetUser , EditProfile, getRandomSuggestions, toggleFollow } from '../controllers/User.Controller.js'
 import { CreatePost, GetAllPosts } from "../controllers/Post.Controller.js";
 import upload from "../middlewares/upload.js";
 import Auth  from "../middlewares/Auth.js";
@@ -9,4 +9,8 @@ router.post('/login', LoginUser)
 router.post('/create',Auth,upload.single('file'),CreatePost)
 router.get('/getAll', GetAllPosts)
 router.get('/logout', Logout)
+router.get('/user/:username', GetUser)
+router.patch("/profile/edit", Auth,  EditProfile);
+router.get('/suggestion',getRandomSuggestions)   
+router.post('/:userId/followUnfollow', Auth, toggleFollow);
 export default router;
--- routes/Posts-routes.js
+++ routes/Posts-routes.js
@@ -1,6 +1,12 @@
-// import express from 'express';
-// import upload from '../middlewares/upload.js';
-// import { CreatePost } from '../controllers/Post.Controller.js';
-// const router = express.Router();
-// export default router;
+import express from 'express';
+import { AddComment, getPostById, LikeAndDislikePost , GetAllPosts , GetMyPost} from '../controllers/Post.Controller.js';
+import Auth from '../middlewares/Auth.js';
+const router = express.Router();
+router.get('/myposts', Auth, GetMyPost)
+router.get('/:Id', Auth,getPostById);
+router.post("/:Id/comment", Auth, AddComment);
+router.post("/:Id/Like", Auth, LikeAndDislikePost);
+router.get("/" ,GetAllPosts)
+
+export default router
 
